72ca5876f553f595582993e800708b707b956b16
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2015-02-20 15:20:03 +0100

    Support for Internet Explorer 8

--- EventTarget.js
+++ EventTarget.js
@@ -3,51 +3,57 @@
  * @author Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
  */
 
+
 function EventTarget()
 {
   var listeners = {};
 
   this.addEventListener = function(type, listener)
   {
-		if(!listener) return
+    if(!listener) return
 
-		var listeners_type = listeners[type]
+    var listeners_type = listeners[type]
     if(listeners_type === undefined)
       listeners[type] = listeners_type = [];
 
-    if(listeners_type.indexOf(listener) === -1)
-      listeners_type.push(listener);
+    for(var i=0,l; l=listeners_type[i]; i++)
+      if(l === listener) return;
+
+    listeners_type.push(listener);
   };
 
   this.dispatchEvent = function(event)
   {
-		var type = event.type
+    var type = event.type
     var listenerArray = (listeners[type] || []);
 
     var dummyListener = this['on' + type];
     if(typeof dummyListener == 'function')
       listenerArray = listenerArray.concat(dummyListener);
 
-		for(var i=0,listener; listener=listenerArray[i]; i++)
+    for(var i=0,listener; listener=listenerArray[i]; i++)
       listener.call(this, event);
   };
 
   this.removeEventListener = function(type, listener)
   {
-		if(!listener) return
+    if(!listener) return
 
-		var listeners_type = listeners[type]
-		if(listeners_type === undefined) return
+    var listeners_type = listeners[type]
+    if(listeners_type === undefined) return
 
-    var index = listeners_type.indexOf(listener);
-    if(index !== -1)
-      listeners_type.splice(index, 1);
+    for(var i=0,l; l=listeners_type[i]; i++)
+      if(l === listener)
+      {
+        listeners_type.splice(i, 1);
+        break;
+      }
 
-		if(!listeners_type.length)
-			delete listeners[type]
+    if(!listeners_type.length)
+      delete listeners[type]
   };
 };
 
 
 if(typeof module !== 'undefined' && module.exports)
-	module.exports = EventTarget;
+  module.exports = EventTarget;
