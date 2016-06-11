8f79a2643a0fb82d2e9528975ac69f1c8c668277
Brett Zamir <brettz9@yahoo.com>
2016-06-11 13:54:32 -0700

    Add add/remove/has *EarlyEventListener/*LateEventListener (incomplete); Full Node support; Code maintenance: Add standard ESLint config;

--- EventTarget.js
+++ EventTarget.js
@@ -1,79 +1,25 @@
+var DOMException;
+(function () {
+  'use strict'
 
-function EventTarget() {}
+  if (typeof window === 'undefined') {
+    DOMException = function (msg, name) {
+      return new Error(name + ': ' + msg)
+    }
+  }
 
-Object.assign(EventTarget.prototype, {
-
-  addEventListener: function (type, listener) {
-    if (!this._listeners) Object.defineProperty(this, '_listeners', {value: {}})
-    var listeners = this._listeners
-
+  function addListener (listeners, listener, type) {
     var listenersByType = listeners[type]
-    if (listenersByType === undefined) listeners[type] = listenersByType = [];
+    if (listenersByType === undefined) listeners[type] = listenersByType = []
 
     if (listenersByType.some(function (l) {
       return l === listener
     })) return
 
     listenersByType.push(listener)
-  },
+  }
 
-  dispatchEvent: function (ev) {
-    if (!this._listeners) Object.defineProperty(this, '_listeners', {value: {}})
-    var listeners = this._listeners
-    if (ev._dispatched) throw new DOMException('The object is in an invalid state.', 'InvalidStateError')
-    ev._dispatched = true
-
-    var type = ev.type
-    if (type == undefined || type == '') throw new DOMException('UNSPECIFIED_EVENT_TYPE_ERR', 'UNSPECIFIED_EVENT_TYPE_ERR')
-
-    var listenersByType = listeners[type].concat() || []
-
-    var dummyListener = this['on' + type]
-    var dummyIPos = listenersByType.length ? 1 : 0
-
-    var stopImmediatePropagation = false
-
-    // [ToDo] Use read-only properties instead of attributes when available
-    ev.cancelable = true
-    ev.defaultPrevented = false
-    ev.isTrusted = false
-    ev.preventDefault = function () {
-      if (this.cancelable) this.defaultPrevented = true
-    }
-    ev.stopImmediatePropagation = function () {
-      stopImmediatePropagation = true
-    }
-    ev.target = this
-    ev.timeStamp = new Date().getTime()
-
-    listenersByType.some(function (listener, i) {
-      if (stopImmediatePropagation) return true
-      if (i === dummyIPos && typeof dummyListener === 'function') {
-        // We don't splice this in as could be overwritten; executes here per
-        //  https://html.spec.whatwg.org/multipage/webappapis.html#event-handler-attributes:event-handlers-14
-        dummyListener.call(this, ev)
-      }
-      listener.call(this, ev)
-    }, this)
-    if (typeof dummyListener === 'function' && listenersByType.length < 2) dummyListener.call(this, ev) // Won't have executed if too short
-
-    return !ev.defaultPrevented
-  },
-
-  hasEventListener: function (type, listener) {
-    if (!this._listeners) Object.defineProperty(this, '_listeners', {value: {}})
-
-    var listeners = this._listeners;
-    if (listeners[type] !== undefined && listeners[type].indexOf(listener) !== - 1) {
-      return true
-    }
-    return false
-  },
-
-  removeEventListener: function (type, listener) {
-    if (!this._listeners) Object.defineProperty(this, '_listeners', {value: {}})
-    var listeners = this._listeners
-
+  function removeListener (listeners, listener, type) {
     var listenersByType = listeners[type]
     if (listenersByType === undefined) return
 
@@ -86,6 +32,104 @@ Object.assign(EventTarget.prototype, {
 
     if (!listenersByType.length) delete listeners[type]
   }
-})
 
-if (typeof module !== 'undefined' && module.exports) module.exports = EventTarget
+  function hasListener (listeners, listener, type) {
+    return listeners[type] !== undefined && listeners[type].indexOf(listener) !== -1
+  }
+
+  function EventTarget () {}
+
+  Object.assign(EventTarget.prototype, {
+
+    // This should only be used within an implementation
+    addEarlyEventListener: function (type, listener) {
+      if (!this._earlyListeners) Object.defineProperty(this, '_earlyListeners', {value: {}})
+      addListener(this._earlyListeners, listener, type)
+    },
+    // This should only be used within an implementation
+    removeEarlyEventListener: function (type, listener) {
+      if (!this._earlyListeners) Object.defineProperty(this, '_earlyListeners', {value: {}})
+      removeListener(this._earlyListeners, listener, type)
+    },
+    hasEarlyEventListener: function (type, listener) {
+      if (!this._earlyListeners) Object.defineProperty(this, '_earlyListeners', {value: {}})
+      return hasListener(this._earlyListeners, listener, type)
+    },
+
+    // This should only be used within an implementation
+    addLateEventListener: function (type, listener) {
+      if (!this._lateListeners) Object.defineProperty(this, '_lateListeners', {value: {}})
+      addListener(this._lateListeners, listener, type)
+    },
+    // This should only be used within an implementation
+    removeLateEventListener: function (type, listener) {
+      if (!this._lateListeners) Object.defineProperty(this, '_lateListeners', {value: {}})
+      removeListener(this._lateListeners, listener, type)
+    },
+    hasLateEventListener: function (type, listener) {
+      if (!this._lateListeners) Object.defineProperty(this, '_lateListeners', {value: {}})
+      return hasListener(this._lateListeners, listener, type)
+    },
+
+    addEventListener: function (type, listener) {
+      if (!this._listeners) Object.defineProperty(this, '_listeners', {value: {}})
+      addListener(this._listeners, listener, type)
+    },
+    removeEventListener: function (type, listener) {
+      if (!this._listeners) Object.defineProperty(this, '_listeners', {value: {}})
+      removeListener(this._listeners, listener, type)
+    },
+    hasEventListener: function (type, listener) {
+      if (!this._listeners) Object.defineProperty(this, '_listeners', {value: {}})
+      return hasListener(this._listeners, listener, type)
+    },
+
+    dispatchEvent: function (ev) {
+      if (!this._listeners) Object.defineProperty(this, '_listeners', {value: {}})
+      if (!this._earlyListeners) Object.defineProperty(this, '_earlyListeners', {value: {}})
+      if (!this._lateListeners) Object.defineProperty(this, '_lateListeners', {value: {}})
+
+      if (ev._dispatched) throw new DOMException('The object is in an invalid state.', 'InvalidStateError')
+      ev._dispatched = true
+
+      var type = ev.type
+      if (type == undefined || type == '') throw new DOMException('UNSPECIFIED_EVENT_TYPE_ERR', 'UNSPECIFIED_EVENT_TYPE_ERR') // eslint-disable-line eqeqeq
+
+      var listenersByType = this._listeners[type].concat() || []
+
+      var onListener = this['on' + type]
+      var dummyIPos = listenersByType.length ? 1 : 0
+
+      var stopImmediatePropagation = false
+
+      // [ToDo] Use read-only properties instead of attributes when available
+      ev.cancelable = true
+      ev.defaultPrevented = false
+      ev.isTrusted = false
+      ev.preventDefault = function () {
+        if (this.cancelable) this.defaultPrevented = true
+      }
+      ev.stopImmediatePropagation = function () {
+        stopImmediatePropagation = true
+      }
+      ev.target = this
+      ev.timeStamp = new Date().getTime()
+
+      listenersByType.some(function (listener, i) {
+        if (stopImmediatePropagation) return true
+        if (i === dummyIPos && typeof onListener === 'function') {
+          // We don't splice this in as could be overwritten; executes here per
+          //  https://html.spec.whatwg.org/multipage/webappapis.html#event-handler-attributes:event-handlers-14
+          onListener.call(this, ev)
+        }
+        listener.call(this, ev)
+      }, this)
+      if (typeof onListener === 'function' && listenersByType.length < 2) onListener.call(this, ev) // Won't have executed if too short
+
+      return !ev.defaultPrevented
+    }
+  })
+
+  if (typeof module !== 'undefined' && module.exports) module.exports = EventTarget
+  else window.EventTarget = EventTarget
+}())
--- README.md
+++ README.md
@@ -2,10 +2,27 @@
 
 JavaScript events for custom objects
 
-## Usage
+## Installation
+
+### Browser
 
 ```html
 <script src="EventTarget.js"></script>
+```
+
+### Node
+
+```shell
+npm install
+```
+
+```js
+var EventTarget = require('eventtarget');
+```
+
+## Usage
+
+```html
 <script>
 
 // Adding events to custom object
@@ -30,3 +47,50 @@ car.start();
 
 </script>
 ```
+
+## Standard methods
+
+The following behave as with the standard `EventTarget` methods
+(as possible).
+
+-   `addEventListener(type, listener)`
+-   `removeEventListener(type, listener)`
+-   `dispatchEvent(ev)`
+-   `on* = function (ev) {};` (e.g., `onclick`)
+
+## Custom methods
+
+The following are non-standard methods:
+
+-   `hasEventListener(type, listener)` - Detects whether a given event
+    listener exists
+
+-   `<add/remove/has>EarlyEventListener` - Allows maintenance of functions
+    which execute prior to all other event listeners. Should be used
+    within implementations only to avoid unexpected behavior by consumers.
+    These should generally not stop propagation or prevent the default
+    behavior since that should normally be left up to the consumer.
+
+-   `<add/remove/has>LateEventListener` - Allows maintenance of functions
+    which execute subsequent to all other event listeners. Should be used
+    within implementations only to avoid unexpected behavior by consumers.
+
+## Todos
+
+-   Use babel and ES6 Symbols, e.g., for bubbling state, parents (or
+    getParent), parent names/paths, etc.
+
+-   `on*` to `preventDefault` with `return false`.
+
+-   Demo `click()` (for capturing/bubbling/propagation) and
+    `submit()` (for default and default prevention) on implementations on JSON.
+
+-   Complete `<add/remove/has>earlyEventListener` and
+    `<add/remove/has>LateEventListener` behaviors in `dispatchEvent`.
+
+-   Designate and document properties for capturing/bubbling/propagation.
+
+-   Add add/remove/has defaultEventListener
+    (You can check the `defaultPrevented` property in these methods to
+    determine whether the user has called `e.preventDefault()` and thus to
+    decide whether to continue on with some default behavior.)
