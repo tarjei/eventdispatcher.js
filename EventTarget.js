d829dd4e65f27720dc1637fb8d2cc4f7fd647de3
Brett Zamir <brettz9@yahoo.com>
2016-07-08 14:24:15 -0700

    Refactor

--- EventTarget.js
+++ EventTarget.js
@@ -1,135 +1,110 @@
 var DOMException;
 (function () {
-  'use strict'
+  'use strict';
 
   if (typeof window === 'undefined') {
     DOMException = function (msg, name) {
-      return new Error(name + ': ' + msg)
-    }
+      return new Error(name + ': ' + msg);
+    };
   }
 
   function addListener (listeners, listener, type) {
-    var listenersByType = listeners[type]
-    if (listenersByType === undefined) listeners[type] = listenersByType = []
+    var listenersByType = listeners[type];
+    if (listenersByType === undefined) listeners[type] = listenersByType = [];
 
     if (listenersByType.some(function (l) {
-      return l === listener
-    })) return
+      return l === listener;
+    })) return;
 
-    listenersByType.push(listener)
+    listenersByType.push(listener);
   }
 
   function removeListener (listeners, listener, type) {
-    var listenersByType = listeners[type]
-    if (listenersByType === undefined) return
+    var listenersByType = listeners[type];
+    if (listenersByType === undefined) return;
 
     listenersByType.some(function (l, i) {
       if (l === listener) {
-        listenersByType.splice(i, 1)
-        return true
+        listenersByType.splice(i, 1);
+        return true;
       }
-    })
+    });
 
-    if (!listenersByType.length) delete listeners[type]
+    if (!listenersByType.length) delete listeners[type];
   }
 
   function hasListener (listeners, listener, type) {
-    return listeners[type] !== undefined && listeners[type].indexOf(listener) !== -1
+    return listeners[type] !== undefined && listeners[type].indexOf(listener) !== -1;
   }
 
   function EventTarget () {}
 
+  Object.assign(EventTarget.prototype, ['Early', '', 'Late', 'Default'].map(function (obj, listenerType) {
+    obj['add' + listenerType + 'EventListener'] = function (type, listener) {
+      var arrStr = '_' + listenerType.toLowerCase() + 'Listeners';
+      if (!this[arrStr]) Object.defineProperty(this, arrStr, {value: {}});
+      addListener(this[arrStr], listener, type);
+    };
+    obj['remove' + listenerType + 'EventListener'] = function (type, listener) {
+      var arrStr = '_' + listenerType.toLowerCase() + 'Listeners';
+      if (!this[arrStr]) Object.defineProperty(this, arrStr, {value: {}});
+      removeListener(this[arrStr], listener, type);
+    };
+    obj['has' + listenerType + 'EventListener'] = function (type, listener) {
+      var arrStr = '_' + listenerType.toLowerCase() + 'Listeners';
+      if (!this[arrStr]) Object.defineProperty(this, arrStr, {value: {}});
+      return hasListener(this[arrStr], listener, type);
+    };
+  }));
+
   Object.assign(EventTarget.prototype, {
 
-    // This should only be used within an implementation
-    addEarlyEventListener: function (type, listener) {
-      if (!this._earlyListeners) Object.defineProperty(this, '_earlyListeners', {value: {}})
-      addListener(this._earlyListeners, listener, type)
-    },
-    // This should only be used within an implementation
-    removeEarlyEventListener: function (type, listener) {
-      if (!this._earlyListeners) Object.defineProperty(this, '_earlyListeners', {value: {}})
-      removeListener(this._earlyListeners, listener, type)
-    },
-    hasEarlyEventListener: function (type, listener) {
-      if (!this._earlyListeners) Object.defineProperty(this, '_earlyListeners', {value: {}})
-      return hasListener(this._earlyListeners, listener, type)
-    },
-
-    // This should only be used within an implementation
-    addLateEventListener: function (type, listener) {
-      if (!this._lateListeners) Object.defineProperty(this, '_lateListeners', {value: {}})
-      addListener(this._lateListeners, listener, type)
-    },
-    // This should only be used within an implementation
-    removeLateEventListener: function (type, listener) {
-      if (!this._lateListeners) Object.defineProperty(this, '_lateListeners', {value: {}})
-      removeListener(this._lateListeners, listener, type)
-    },
-    hasLateEventListener: function (type, listener) {
-      if (!this._lateListeners) Object.defineProperty(this, '_lateListeners', {value: {}})
-      return hasListener(this._lateListeners, listener, type)
-    },
-
-    addEventListener: function (type, listener) {
-      if (!this._listeners) Object.defineProperty(this, '_listeners', {value: {}})
-      addListener(this._listeners, listener, type)
-    },
-    removeEventListener: function (type, listener) {
-      if (!this._listeners) Object.defineProperty(this, '_listeners', {value: {}})
-      removeListener(this._listeners, listener, type)
-    },
-    hasEventListener: function (type, listener) {
-      if (!this._listeners) Object.defineProperty(this, '_listeners', {value: {}})
-      return hasListener(this._listeners, listener, type)
-    },
-
     dispatchEvent: function (ev) {
-      if (!this._listeners) Object.defineProperty(this, '_listeners', {value: {}})
-      if (!this._earlyListeners) Object.defineProperty(this, '_earlyListeners', {value: {}})
-      if (!this._lateListeners) Object.defineProperty(this, '_lateListeners', {value: {}})
+      if (!this._listeners) Object.defineProperty(this, '_listeners', {value: {}});
+      if (!this._earlyListeners) Object.defineProperty(this, '_earlyListeners', {value: {}});
+      if (!this._lateListeners) Object.defineProperty(this, '_lateListeners', {value: {}});
 
-      if (ev._dispatched) throw new DOMException('The object is in an invalid state.', 'InvalidStateError')
-      ev._dispatched = true
+      if (ev._dispatched) throw new DOMException('The object is in an invalid state.', 'InvalidStateError');
+      ev._dispatched = true;
 
-      var type = ev.type
-      if (type == undefined || type == '') throw new DOMException('UNSPECIFIED_EVENT_TYPE_ERR', 'UNSPECIFIED_EVENT_TYPE_ERR') // eslint-disable-line eqeqeq
+      var type = ev.type;
+      if (type == undefined || type == '') throw new DOMException('UNSPECIFIED_EVENT_TYPE_ERR', 'UNSPECIFIED_EVENT_TYPE_ERR'); // eslint-disable-line eqeqeq
 
-      var listenersByType = this._listeners[type].concat() || []
+      var listenersByType = this._listeners[type].concat() || [];
 
-      var onListener = this['on' + type]
-      var dummyIPos = listenersByType.length ? 1 : 0
+      var onListener = this['on' + type];
+      var dummyIPos = listenersByType.length ? 1 : 0;
 
-      var stopImmediatePropagation = false
+      var stopImmediatePropagation = false;
 
       // [ToDo] Use read-only properties instead of attributes when available
-      ev.cancelable = true
-      ev.defaultPrevented = false
-      ev.isTrusted = false
+      ev.cancelable = true;
+      ev.defaultPrevented = false;
+      ev.isTrusted = false;
       ev.preventDefault = function () {
-        if (this.cancelable) this.defaultPrevented = true
-      }
+        if (this.cancelable) this.defaultPrevented = true;
+      };
       ev.stopImmediatePropagation = function () {
-        stopImmediatePropagation = true
-      }
-      ev.target = this
-      ev.timeStamp = new Date().getTime()
+        stopImmediatePropagation = true;
+      };
+      ev.target = this;
+      ev.timeStamp = new Date().getTime();
 
       listenersByType.some(function (listener, i) {
-        if (stopImmediatePropagation) return true
+        if (stopImmediatePropagation) return true;
         if (i === dummyIPos && typeof onListener === 'function') {
           // We don't splice this in as could be overwritten; executes here per
           //  https://html.spec.whatwg.org/multipage/webappapis.html#event-handler-attributes:event-handlers-14
-          onListener.call(this, ev)
+          onListener.call(this, ev);
         }
-        listener.call(this, ev)
-      }, this)
-      if (typeof onListener === 'function' && listenersByType.length < 2) onListener.call(this, ev) // Won't have executed if too short
+        listener.call(this, ev);
+      }, this);
+      if (typeof onListener === 'function' && listenersByType.length < 2) onListener.call(this, ev); // Won't have executed if too short
 
-      return !ev.defaultPrevented
+      return !ev.defaultPrevented;
     }
-  })
+  });
 
-  if (typeof module !== 'undefined' && module.exports) module.exports = EventTarget
-  else window.EventTarget = EventTarget
-}())
+  if (typeof module !== 'undefined' && module.exports) module.exports = EventTarget;
+  else window.EventTarget = EventTarget;
+}());
--- README.md
+++ README.md
@@ -66,31 +66,34 @@ The following are non-standard methods:
     listener exists
 
 -   `<add/remove/has>EarlyEventListener` - Allows maintenance of functions
-    which execute prior to all other event listeners. Should be used
-    within implementations only to avoid unexpected behavior by consumers.
-    These should generally not stop propagation or prevent the default
-    behavior since that should normally be left up to the consumer.
+    which execute prior to all other event listeners. These should generally
+    not stop propagation or prevent the default behavior since that should
+    normally be left up to the consumer.
+
+-   `<add/remove/has>DefaultEventListener` - Allows maintenance of functions
+    which execute subsequent to all normal event listeners and which can be
+    prevented via `preventDefault` (or checked via `defaultPrevented`).
+    These events will not be stopped by `stopPropagation` or
+    `stopImmediatePropagation` alone. (You can check the `defaultPrevented`
+    property in these methods to determine whether the user has called
+    `e.preventDefault()` and thus to decide whether to continue on with some
+    default behavior.)
 
 -   `<add/remove/has>LateEventListener` - Allows maintenance of functions
-    which execute subsequent to all other event listeners. Should be used
+    which execute subsequent to all event listeners. Should be used
     within implementations only to avoid unexpected behavior by consumers.
 
 ## Todos
-
--   Use babel and ES6 Symbols, e.g., for bubbling state, parents (or
-    getParent), parent names/paths, etc.
-
--   `on*` to `preventDefault` with `return false`.
-
--   Demo `click()` (for capturing/bubbling/propagation) and
-    `submit()` (for default and default prevention) on implementations on JSON.
 
 -   Complete `<add/remove/has>earlyEventListener` and
     `<add/remove/has>LateEventListener` behaviors in `dispatchEvent`.
 
 -   Designate and document properties for capturing/bubbling/propagation.
 
--   Add add/remove/has defaultEventListener
-    (You can check the `defaultPrevented` property in these methods to
-    determine whether the user has called `e.preventDefault()` and thus to
-    decide whether to continue on with some default behavior.)
+-   `on*` to `preventDefault` with `return false`.
+
+-   Use babel and ES6 Symbols, e.g., for bubbling state, parents (or
+    getParent), parent names/paths, etc. to provide namespacing?
+
+-   Demo `click()` (for capturing/bubbling/propagation) and
+    `submit()` (for default and default prevention) on implementations on JSON.
